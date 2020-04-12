8000bf5327689cf6ecb3ef7c779ef3037e2e3fee
damqnn <51029651+damqnn@users.noreply.github.com>
2020-04-12 18:58:33 +0300

    Add files via upload

---
+++ Shop.Application/Products/GetProducts.cs
@@ -0,0 +1,35 @@
+﻿using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Products
+{
+    public class GetProducts
+    {
+        
+        private ApplicationDbContext _ctx;
+
+        public GetProducts(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IEnumerable<ProductViewModels> Do() =>
+            _ctx.Products.ToList().Select(x => new ProductViewModels
+            {
+                Name = x.Name,
+                Desctription = x.Desctription,
+                Value = $"$ {x.Value.ToString("N2")}"
+            });
+        public class ProductViewModels
+        {
+            public string Name { get; set; }
+
+            public string Desctription { get; set; }
+
+            public string Value { get; set; }
+        }
+    }
+}
---
+++ Shop.Application/ProductsAdmin/UpdateProduct.cs
@@ -0,0 +1,59 @@
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.ProductsAdmin
+{
+    public class UpdateProduct
+    {
+        private ApplicationDbContext _context;
+
+        public UpdateProduct(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response> Do(Request request)
+        {
+            var product = _context.Products.FirstOrDefault(x => x.Id == request.Id);
+
+            product.Name = request.Name;
+            product.Desctription = request.Desctription;
+            product.Value = request.Value;
+
+            await _context.SaveChangesAsync();
+            return new Response
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Desctription = product.Desctription,
+                Value = product.Value
+            } ;
+        }
+
+
+        public class Request
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            public string Desctription { get; set; }
+
+            public decimal Value { get; set; }
+        }
+        public class Response
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            public string Desctription { get; set; }
+
+            public decimal Value { get; set; }
+        }
+    }
+}
+
