092c74bbba02675839d3a64e2a63c90ab1fef7f3
damqnn <51029651+damqnn@users.noreply.github.com>
2020-04-15 15:39:39 +0300

    Add files via upload

--- Shop.Application/Cart/AddCustomerInformation.cs
+++ Shop.Application/Cart/AddCustomerInformation.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using Shop.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -22,23 +23,42 @@ namespace Shop.Application.Cart
 
         public class Request
         {
-            [Required]
+
+            [Required(ErrorMessage = "{0} is required")]
+            [StringLength(100, MinimumLength = 3,
+                ErrorMessage = "FirstName Should be minimum 3 characters and a maximum of 100 characters")]
+            [RegularExpression("^((?!^First Name$)[a-zA-Z '])+$", 
+                ErrorMessage = "First name is required and must be properly formatted.")]
+            [DataType(DataType.Text)]
             public string FirstName { get; set; }
-            [Required]
+            [Required(ErrorMessage = "{0} is required")]
+            [StringLength(100, MinimumLength = 3,
+            ErrorMessage = "LastName Should be minimum 3 characters and a maximum of 100 characters")]
+            [RegularExpression("^((?!^Last Name$)[a-zA-Z '])+$",
+                ErrorMessage = "Last name is required and must be properly formatted.")]
+            [DataType(DataType.Text)]
+
             public string LastName { get; set; }
-            [Required]
+            [Required(ErrorMessage = "{0} is required")]
             [DataType(DataType.EmailAddress)]
             public string Email { get; set; }
-            [Required]
+            [Required(ErrorMessage = "{0} is required")]
             [DataType(DataType.PhoneNumber)]
             public string PhoneNumber { get; set; }
-            [Required]
+            [Required(ErrorMessage = "Address is required")]
+            [RegularExpression("^((?!^Address$)[0-9A-Za-z #.,])+$", 
+                ErrorMessage = "Address is required and must be properly formatted.")]
             public string Address1 { get; set; }
-            
+            [RegularExpression("^((?!^Address$)[0-9A-Za-z #.,])+$", 
+                ErrorMessage = "Address is required and must be properly formatted.")]
+
             public string Address2 { get; set; }
-            [Required]
+            [Required(ErrorMessage = "City is a Required field.")]
+            [DataType(DataType.Text)]
+            [RegularExpression("^((?!^City$)[a-zA-Z '])+$", ErrorMessage = "City is required and must be properly formatted.")]
+           
             public string City { get; set; }
-            [Required]
+            [Required(ErrorMessage = "The {0} is required")]
             public string PostCode { get; set; }
 
         }
